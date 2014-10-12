15c51e97a063ed0a6bd62657b936b0b6e1753a8e
zinglax <dylanzingler@gmail.com>
2014-10-12 13:15:56 -0400

    working on changing the background

--- dylansSITE/home/urls.py
+++ dylansSITE/home/urls.py
@@ -3,4 +3,4 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('dataloader.views',
     #url(r'^investing/$', 'investing', name='investing'),
-)
+)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
